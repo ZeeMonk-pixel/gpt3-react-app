1e013bb9e8f19e7d1c0372fe59ccfc81091c5688
ZeeMonk-pixel <theyoungachiever18@yahoo.com>
2022-11-22 00:26:30 +0100

    edits

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Footer, Blog, Possibility, Features, WhatsGPT3, Header } from './containers';
+import { Cta, Brand, Navbar } from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+        <div className='gradient_bg'>
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatsGPT3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
